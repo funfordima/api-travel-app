33432720713fbb631bd914bef8edee76b4bd28bc
funfordima <d.g.litvinov@gmail.com>
2021-03-09 17:26:25 +0200

    feat: test first API users.json

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
+const filePath = `${path.resolve(__dirname)}/routes/users.json`;
+
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  const content = fs.readFileSync(filePath, 'utf-8');
+  const users = JSON.parse(content);
+
+  res.send(users);
+});
 
 export default app;
