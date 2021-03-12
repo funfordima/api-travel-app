94d132398aae48b9e65d1e6d38a1707a14eb1dd9
funfordima <d.g.litvinov@gmail.com>
2021-03-12 18:57:53 +0200

    feat: add placeRouter, add handle errors

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,32 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import countryRouter from './routes/countryRouter';
+import placeRouter from './routes/placeRouter';
 
 const app = express();
 
-// app.use(logger('common', {
-//   stream: fs.createWriteStream('./access.log', {flags: 'a'})
-// }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/countries', countryRouter);
+app.use('/places', placeRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  const status = 404;
+  next({ 
+    error,
+    status
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.json({
+    statusCode: err.status || 505,
+    message:  err.error.message,
+  })
+});
 
 export default app;
---
+++ src/constants/constants.ts
@@ -0,0 +1,2 @@
+export const countries = 'countries';
+export const places = 'places';
